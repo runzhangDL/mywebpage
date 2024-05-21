ca37a8396fc56742be2f53b116033800317f39ed
runzhang <runzhang715@gmail.com>
2024-05-21 17:30:54 +0800

    my web page

---
+++ pages/gear.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Home.module.css';
+
+// const Gear = () => {
+//   const [scrollDirection, setScrollDirection] = useState(null);
+
+//   useEffect(() => {
+//     let lastScrollPosition = window.scrollY;
+
+//     const handleScroll = () => {
+//       const currentScrollPosition = window.scrollY;
+//       const direction = currentScrollPosition > lastScrollPosition ? 'down' : 'up';
+//       setScrollDirection(direction);
+//       lastScrollPosition = currentScrollPosition;
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       className={styles.gear}
+//       style={{
+//         transform: scrollDirection === 'down' ? 'rotate(45deg)' : 'rotate(-45deg)',
+//       }}
+//     >
+//     </div>
+//   );
+// };
+
+const Gear = () => {
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+      const rotationAngle = currentScrollPosition * 0.1;
+      setRotation(rotationAngle);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={styles.gear}
+      style={{
+        transform: `rotate(${rotation}deg)`,
+      }}
+    ></div>
+  );
+};
+
+
+export default Gear;
