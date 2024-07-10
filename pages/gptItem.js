3cd18a913ddc564d4fdb94676701350c05cf48e7
runzhangDL <runzhang715@gmail.com>
2024-07-10 10:22:44 -0400

    add english polishing project

--- pages/gptItem.js
+++ pages/gptItem.js
@@ -8,7 +8,7 @@ const GptItem = () => {
         <a className={styles.project} href="http://ec2-18-214-100-128.compute-1.amazonaws.com:3000/" target='_blank'>
           <img src='/gpt.png'></img>
           <div>
-            <h4>A English polishing tool based on ChatGPT</h4>
+            <h4>A ChatGPT-powered AI tool for refining English writing</h4>
             <p>This full-stack English polishing tool utilizes Spring Boot for RESTful API development and integration. The front-end is crafted with React and styled using Tailwind CSS. Deployed on Amazon Web Services (AWS), the project uses AWS S3 for persistent storage. The ChatGPT API serves as the primary engine powering the polishing algorithm.</p>
           </div>
         </a>
